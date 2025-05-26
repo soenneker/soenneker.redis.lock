5359043f2dc754e89068b966f2f400791c5e9f16
Jake Soenneker <jake@soeneker.com>
2025-05-26 23:38:42 +0000

    FluentAssertions -> AwesomeAssertions namespace

--- test/Soenneker.Redis.Lock.Tests/RedisLockUtilTests.cs
+++ test/Soenneker.Redis.Lock.Tests/RedisLockUtilTests.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using FluentAssertions;
+using AwesomeAssertions;
 using Soenneker.Redis.Lock.Abstract;
 using Soenneker.Tests.FixturedUnit;
 using Xunit;
