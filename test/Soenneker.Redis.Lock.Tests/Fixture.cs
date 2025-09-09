df9ac8dff22579f98974cf858e89ed3d925f2be2
Jake Soenneker <jake@soeneker.com>
2025-09-09 16:06:04 -0500

    Fixes logger disposal

--- test/Soenneker.Redis.Lock.Tests/Fixture.cs
+++ test/Soenneker.Redis.Lock.Tests/Fixture.cs
@@ -21,7 +21,7 @@ public class Fixture : UnitFixture
     {
         services.AddLogging(builder =>
         {
-            builder.AddSerilog(dispose: true);
+            builder.AddSerilog(dispose: false);
         });
 
         IConfiguration config = TestUtil.BuildConfig();
